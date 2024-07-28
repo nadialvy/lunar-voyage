43f9bb1dad9f8d3d38d918f2de46ef05a1024d7c
nadialvy <5026231007@student.its.ac.id>
2024-07-29 01:02:25 +0700

    feat: init project

---
+++ lunar-voyage/Models/Apod.swift
@@ -0,0 +1,18 @@
+//
+//  Apod.swift
+//  lunar-voyage
+//
+//  Created by Nadia Lovely on 29/07/24.
+//
+
+import Foundation
+
+struct Apod {
+    var date: String
+    var explanation: String
+    var media_type: String
+    var service_version: String
+    var thumbnail_url: String
+    var title: String
+    var url: String
+}
---
+++ lunar-voyage/Models/ApodManager.swift
@@ -0,0 +1,16 @@
+//
+//  ApodManager.swift
+//  lunar-voyage
+//
+//  Created by Nadia Lovely on 29/07/24.
+//
+
+import Foundation
+
+class ApodManager {
+    var apod: Apod = Apod(date: "", explanation: "", media_type: "", service_version: "", thumbnail_url: "", title: "", url: "")
+    
+    func getApod(){
+        
+    }
+}

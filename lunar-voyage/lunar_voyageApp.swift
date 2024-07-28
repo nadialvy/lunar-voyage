549e6995919fbff933029244d3828c984e2b719c
nadialvy <5026231007@student.its.ac.id>
2024-07-29 00:18:26 +0700

    Initial Commit

---
+++ lunar-voyage/lunar_voyageApp.swift
@@ -0,0 +1,17 @@
+//
+//  lunar_voyageApp.swift
+//  lunar-voyage
+//
+//  Created by Nadia Lovely on 29/07/24.
+//
+
+import SwiftUI
+
+@main
+struct lunar_voyageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

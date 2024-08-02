d46424f4f8b4cf9dc4c4296bdc03aac4212ee51a
nadialvy <5026231007@student.its.ac.id>
2024-08-02 23:02:51 +0700

    feat: add planet model

---
+++ lunar-voyage/Models/Planet.swift
@@ -0,0 +1,20 @@
+//
+//  Planet.swift
+//  lunar-voyage
+//
+//  Created by Nadia Lovely on 02/08/24.
+//
+
+import Foundation
+
+struct Planet {
+    var name: String
+    var mass: Float
+    var radius: Float
+    var period: Int
+    var semi_major_axis: Float
+    var temperature: Int
+    var distance_light_years: Float
+    var host_star_mass: Int
+    var host_star_temperature: Int
+}
